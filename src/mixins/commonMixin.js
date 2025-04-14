999547a1870e24de600abd1dbc678dd8df443036
ANTUS_DEV\phama <phamantu123@gmail.com>
2025-04-14 16:49:44 +0700

    feat: enhance product categorization and update HomeView layout

--- src/mixins/commonMixin.js
+++ src/mixins/commonMixin.js
@@ -2,6 +2,28 @@ export default {
   data() {
     return {
       urlCallApi: process.env.VUE_APP_API_URL || "http://localhost:8080/",
+      typeProduct: {
+        "productsFeatured": {
+          value: 1,
+          name: "Sản phẩm nổi bật"
+        },
+        "productsSnack": {
+          value: 2,
+          name: "Snack"
+        },
+        "productsSpongeCake": {
+          value: 3,
+          name: "Bánh ống xốp"
+        },
+        "productsFish": {
+          value: 4,
+          name: "Đậu phộng da cá"
+        },
+        "productsSnackBlank": {
+          value: 5,
+          name: "Phôi snack"
+        },
+      }
     };
   },
   methods: {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,8 +13,11 @@ export default {
   },
   data() {
     return {
-      productsSnack: [],
       productsFeatured: [],
+      productsSnack: [],
+      productsSpongeCake: [],
+      productsFish: [],
+      productsSnackBlank: [],
       queryCallApi: {
         type: 1,
       }
@@ -25,8 +28,11 @@ export default {
       axios
         .get(this.urlCallApi + `products`)
         .then((response) => {
-          this.productsFeatured = response.data.filter(product => product.type == 1)
-          this.productsSnack = response.data.filter(product => product.type == 2)
+          this.productsFeatured = response.data.filter(product => product.type == this.typeProduct.productsFeatured.value)
+          this.productsSnack = response.data.filter(product => product.type == this.typeProduct.productsSnack.value)
+          this.productsSpongeCake = response.data.filter(product => product.type == this.typeProduct.productsSpongeCake.value)
+          this.productsFish = response.data.filter(product => product.type == this.typeProduct.productsFish.value)
+          this.productsSnackBlank = response.data.filter(product => product.type == this.typeProduct.productsSnackBlank.value)
         })
         .catch((error) => {
           console.error("Error fetching products:", error);
@@ -45,7 +51,7 @@ export default {
     <div class="body">
       <div class="bg_danhgia">
         <div class="body2" style="padding: 1% 0 2% 0;">
-          <div class="title_page_home"><a href="https://pham-asset.com/san-pham">Sản phẩm nổi bật</a></div>
+          <div class="title_page_home"><a href="#">Sản phẩm nổi bật</a></div>
           <div class="jcarousel2-wrapper">
             <div class="jcarousel2" data-jcarousel="true" data-jcarouselautoscroll="true">
               <ul style="left: -2310px; top: 0px;">
@@ -86,236 +92,78 @@ export default {
         </div>
         <div style="clear:both; height:20px;"></div>
       </div>
-      <!-- ==================== SNACK ============================ -->
-
-
-      <div class="cate_image"><a href="https://pham-asset.com/san-pham/banh-xop-ong-2.html">
+      <div class="cate_image"><a href="#">
           <img width="100%" src="https://pham-asset.com/images/Category/banh-xop-ong-fx44b9oc.jpg" alt="pham asset"></a>
+      </div>
+
+      <!-- ==================== Bánh xốp ống ===================== -->
+      <div class="body" style="margin-top:2%">
+        <div class="title_page_home">
+          <a href="#">Bánh xốp ống</a>
+        </div>
+        <div class="item_product" v-for="product in productsSpongeCake" :key="product.id">
+          <div class="product_image_list">
+            <a href="#"> <img :src="product.img"></a>
+          </div>
+          <div class="product_name" style="font-weight:bold; margin:4% 0">
+            <a href="#"> {{ product.name }}</a>
+          </div>
+          <div class="b_muangay" style="margin-bottom:6%"
+            onclick="window.location='/san-pham/banh-xop-tanoshi-10/banh-xop-nhan-kem-vi-matcha-bac-ha-36.html'">
+            Mua ngay
+          </div>
+        </div>
+        <div style="clear:both; height:20px;"></div>
+      </div>
+
+
+      <!-- ==================== Đậu phộng da cá ================== -->
+      <div class="cate_image">
+        <a href="#">
+          <img width="100%" src="https://pham-asset.com/images/Category/dau-phong-da-ca-b5i7l9gc.jpg"
+            alt="pham asset"></a>
+      </div>
+
+      <div class="body" style="margin-top:2%">
+        <div class="title_page_home"><a href="#">Đậu phộng da cá</a></div>
+        <div class="item_product" v-for="product in productsFish" :key="product.id">
+          <div class="product_image_list">
+            <a href="#">
+              <img :src="product.img"></a>
+          </div>
+          <div class="product_name" style="font-weight:bold; margin:4% 0">
+            <a href="#"> {{ product.name }} </a>
+          </div>
+          <div class="b_muangay" style="margin-bottom:6%"
+            onclick="window.location='/san-pham/dau-phong-da-ca-3/dau-ron-ron-vi-tom-cay-nuong-muoi-ot-38.html'">
+            Mua ngay</div>
+        </div>
+        <div style="clear:both; height:20px;"></div>
       </div>
 
       <div class="body" style="margin-top:2%">
         <div class="title_page_home">
-          <a href="https://pham-asset.com/san-pham/banh-xop-ong-2.html">Bánh xốp ống</a>
+          <a href="#">Phôi Snack</a>
         </div>
-        
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/banh-xop-tanoshi-10/banh-xop-nhan-kem-vi-matcha-bac-ha-36.html"><img
-                src="https://pham-asset.com/images/Products/06-gib5t13m.jpg"></a></div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/banh-xop-tanoshi-10/banh-xop-nhan-kem-vi-matcha-bac-ha-36.html">BÁNH
-              XỐP NHÂN KEM VỊ MATCHA BẠC HÀ</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/banh-xop-tanoshi-10/banh-xop-nhan-kem-vi-matcha-bac-ha-36.html'">Mua
-            ngay</div>
-        </div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/banh-xop-tanoshi-10/banh-xop-nhan-kem-vi-socola-chuoi-35.html"><img
-                src="https://pham-asset.com/images/Products/05-a894o96y.jpg"></a></div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/banh-xop-tanoshi-10/banh-xop-nhan-kem-vi-socola-chuoi-35.html">BÁNH
-              XỐP NHÂN KEM VỊ SOCOLA CHUỐI</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/banh-xop-tanoshi-10/banh-xop-nhan-kem-vi-socola-chuoi-35.html'">Mua
-            ngay</div>
-        </div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/banh-xop-jojo-9/banh-xop-nhan-kem-huong-blueberry-17.html"><img
-                src="https://pham-asset.com/images/Products/147a447c-735e-4cf1-bb81-1902ab1b9e59-y261b282.jpg"></a>
+
+        <div class="item_product" v-for="product in productsSnackBlank" :key="product.id">
+          <div class="product_image_list">
+            <a href="#"><img :src="product.img"></a>
           </div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/banh-xop-jojo-9/banh-xop-nhan-kem-huong-blueberry-17.html">BÁNH
-              XỐP NHÂN KEM HƯƠNG BLUEBERRY</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/banh-xop-jojo-9/banh-xop-nhan-kem-huong-blueberry-17.html'">Mua
-            ngay</div>
-        </div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/banh-xop-jojo-9/banh-xop-nhan-kem-huong-socola-hanh-nhan-16.html"><img
-                src="https://pham-asset.com/images/Products/11df95ba-1232-46c5-afb0-808d7c40cd99-2b79i59l.jpg"></a>
+          <div class="product_name" style="font-weight:bold; margin:4% 0">
+            <a href="#">
+              {{ product.name }}</a>
           </div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/banh-xop-jojo-9/banh-xop-nhan-kem-huong-socola-hanh-nhan-16.html">BÁNH
-              XỐP NHÂN KEM HƯƠNG SÔCÔLA HẠNH NHÂN</a></div>
           <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/banh-xop-jojo-9/banh-xop-nhan-kem-huong-socola-hanh-nhan-16.html'">Mua
-            ngay</div>
-        </div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/banh-xop-jojo-9/banh-xop-ong-nhan-kem-vi-cam-15.html"><img
-                src="https://pham-asset.com/images/Products/img-0212-copy-copy-copy-copy-copy-copy-copy-a9p8sg1a.jpg"></a>
+            onclick="window.location='/san-pham/phoi-snack-4/vien-ca-muc-25.html'">
+            Mua ngay
           </div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/banh-xop-jojo-9/banh-xop-ong-nhan-kem-vi-cam-15.html">BÁNH
-              XỐP ỐNG NHÂN KEM VỊ CAM</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/banh-xop-jojo-9/banh-xop-ong-nhan-kem-vi-cam-15.html'">Mua
-            ngay</div>
-        </div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/banh-xop-jojo-9/banh-xop-ong-nhan-kem-vi-pho-mai-14.html"><img
-                src="https://pham-asset.com/images/Products/banh-xop-pho-mai-merge-1-z5nw5ifu.jpg"></a></div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/banh-xop-jojo-9/banh-xop-ong-nhan-kem-vi-pho-mai-14.html">BÁNH
-              XỐP ỐNG NHÂN KEM VỊ PHÔ MAI</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/banh-xop-jojo-9/banh-xop-ong-nhan-kem-vi-pho-mai-14.html'">Mua
-            ngay</div>
-        </div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/banh-xop-jojo-9/banh-xop-ong-nhan-kem-vi-la-dua-13.html"><img
-                src="https://pham-asset.com/images/Products/b96a733a-c26b-436a-98f1-cefa6281fb85-od3gshb4.jpg"></a>
-          </div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/banh-xop-jojo-9/banh-xop-ong-nhan-kem-vi-la-dua-13.html">BÁNH
-              XỐP ỐNG NHÂN KEM VỊ LÁ DỨA</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/banh-xop-jojo-9/banh-xop-ong-nhan-kem-vi-la-dua-13.html'">Mua
-            ngay</div>
-        </div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/banh-xop-jojo-9/banh-xop-ong-cao-cap-nhan-socola-12.html"><img
-                src="https://pham-asset.com/images/Products/3e0e55c0-a423-462c-a0d6-efd0f248db2c-kg811drb.jpg"></a>
-          </div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/banh-xop-jojo-9/banh-xop-ong-cao-cap-nhan-socola-12.html">BÁNH
-              XỐP ỐNG CAO CẤP NHÂN SÔCÔLA</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/banh-xop-jojo-9/banh-xop-ong-cao-cap-nhan-socola-12.html'">Mua
-            ngay</div>
-        </div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/banh-xop-jojo-9/banh-xop-ong-nhan-kem-vi-matcha-lon-11.html"><img
-                src="https://pham-asset.com/images/Products/d800c912-5524-49d3-95f3-80cd2a4dae75-95zx668g.jpg"></a>
-          </div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/banh-xop-jojo-9/banh-xop-ong-nhan-kem-vi-matcha-lon-11.html">BÁNH
-              XỐP ỐNG NHÂN KEM VỊ MATCHA (lon)</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/banh-xop-jojo-9/banh-xop-ong-nhan-kem-vi-matcha-lon-11.html'">Mua
-            ngay</div>
-        </div>
-        <div style="clear:both; height:20px;"></div>
-      </div>
-      <div class="cate_image"><a href="https://pham-asset.com/san-pham/dau-phong-da-ca-3.html"><img width="100%"
-            src="https://pham-asset.com/images/Category/dau-phong-da-ca-b5i7l9gc.jpg" alt="pham asset"></a></div>
-      <div class="body" style="margin-top:2%">
-        <div class="title_page_home"><a href="https://pham-asset.com/san-pham/dau-phong-da-ca-3.html">Đậu phộng da
-            cá</a></div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/dau-phong-da-ca-3/dau-ron-ron-vi-tom-cay-nuong-muoi-ot-38.html"><img
-                src="https://pham-asset.com/images/Products/angia-sweets-snacks-25-da-nen-hinh-anh-1-98p1297w.jpg"></a>
-          </div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/dau-phong-da-ca-3/dau-ron-ron-vi-tom-cay-nuong-muoi-ot-38.html">ĐẬU
-              RÒN RÒN VỊ TÔM CAY NƯỚNG MUỐI ỚT</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/dau-phong-da-ca-3/dau-ron-ron-vi-tom-cay-nuong-muoi-ot-38.html'">Mua
-            ngay</div>
-        </div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/dau-phong-da-ca-3/dau-phong-da-ca-vi-thit-nuong-21.html"><img
-                src="https://pham-asset.com/images/Products/e3768a9e-8a05-4cd5-a8f3-cdbb959294c3-5g17a215.jpg"></a>
-          </div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/dau-phong-da-ca-3/dau-phong-da-ca-vi-thit-nuong-21.html">ĐẬU
-              PHỘNG DA CÁ VỊ THỊT NƯỚNG</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/dau-phong-da-ca-3/dau-phong-da-ca-vi-thit-nuong-21.html'">Mua
-            ngay</div>
-        </div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/dau-phong-da-ca-3/dau-ron-ron-vi-nuoc-cot-dua-20.html"><img
-                src="https://pham-asset.com/images/Products/angia-sweets-snacks-25-da-nen-hinh-anh-0-1-w10q00tm.jpg"></a>
-          </div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/dau-phong-da-ca-3/dau-ron-ron-vi-nuoc-cot-dua-20.html">ĐẬU
-              RÒN RÒN VỊ NƯỚC CỐT DỪA</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/dau-phong-da-ca-3/dau-ron-ron-vi-nuoc-cot-dua-20.html'">Mua
-            ngay</div>
-        </div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/dau-phong-da-ca-3/dau-phong-nuoc-cot-dua-18.html"><img
-                src="https://pham-asset.com/images/Products/69bf5537-cf2a-42db-83df-4853f169ed4f-11b6g5jh.jpg"></a>
-          </div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/dau-phong-da-ca-3/dau-phong-nuoc-cot-dua-18.html">ĐẬU
-              PHỘNG NƯỚC CỐT DỪA</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/dau-phong-da-ca-3/dau-phong-nuoc-cot-dua-18.html'">Mua
-            ngay</div>
-        </div>
-        <div style="clear:both; height:20px;"></div>
-      </div>
-      <div class="cate_image"><a href="https://pham-asset.com/san-pham/phoi-snack-4.html"></a></div>
-      <div class="body" style="margin-top:2%">
-        <div class="title_page_home"><a href="https://pham-asset.com/san-pham/phoi-snack-4.html">Phôi
-            Snack</a></div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/phoi-snack-4/vien-ca-muc-25.html"><img
-                src="https://pham-asset.com/images/Products/04-8pcgib62.jpg"></a></div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/phoi-snack-4/vien-ca-muc-25.html">VIÊN
-              CÁ
-              MỰC</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/phoi-snack-4/vien-ca-muc-25.html'">Mua
-            ngay</div>
-        </div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/phoi-snack-4/phoi-tom-thai-24.html"><img
-                src="https://pham-asset.com/images/Products/03-23mxpqw6.jpg"></a></div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/phoi-snack-4/phoi-tom-thai-24.html">PHÔI
-              TÔM
-              THÁI</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/phoi-snack-4/phoi-tom-thai-24.html'">Mua
-            ngay</div>
-        </div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/phoi-snack-4/phoi-khoai-tay-que-23.html"><img
-                src="https://pham-asset.com/images/Products/02-21r6e4s4.jpg"></a></div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/phoi-snack-4/phoi-khoai-tay-que-23.html">PHÔI
-              KHOAI TÂY QUE</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/phoi-snack-4/phoi-khoai-tay-que-23.html'">Mua
-            ngay</div>
-        </div>
-        <div class="item_product">
-          <div class="product_image_list"><a
-              href="https://pham-asset.com/san-pham/phoi-snack-4/phoi-rau-cu-qua-22.html"><img
-                src="https://pham-asset.com/images/Products/01-ef679c1z.jpg"></a></div>
-          <div class="product_name" style="font-weight:bold; margin:4% 0"><a
-              href="https://pham-asset.com/san-pham/phoi-snack-4/phoi-rau-cu-qua-22.html">PHÔI
-              RAU CỦ QUẢ</a></div>
-          <div class="b_muangay" style="margin-bottom:6%"
-            onclick="window.location='/san-pham/phoi-snack-4/phoi-rau-cu-qua-22.html'">Mua
-            ngay</div>
         </div>
         <div style="clear:both; height:20px;"></div>
       </div>
     </div>
   </div>
   <BodyForm />
-  <!-- <FooterHome /> -->
   <Copyright />
 </template>
 
